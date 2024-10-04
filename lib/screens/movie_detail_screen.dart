2f03f0c54fde6eae06ddcdbb83b4644281e80ac3
Chris Rhee <0407chan@naver.com>
2024-10-05 01:04:05 +0900

    feat: 상세페이지에 배경추가

--- lib/screens/movie_detail_screen.dart
+++ lib/screens/movie_detail_screen.dart
@@ -9,11 +9,43 @@ class MovieDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: Text(movie.title),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: false,
+        title: const Text(
+          'Back to list',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        iconTheme: const IconThemeData(color: Colors.white), // 뒤로가기 버튼 색상 변경
       ),
-      body: Center(
-        child: Text('영화 상세 정보: ${movie.title}'),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+              'https://image.tmdb.org/t/p/original${movie.backdropPath}',
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.black.withOpacity(0.2),
+            ),
+            Center(
+              child: Text(
+                '영화 상세 정보: ${movie.title}',
+                style: const TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
